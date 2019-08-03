e4c6233d283190f38231643f6834b06f722520a5
Rodion Borisov <vintproykt@gmail.com>
2019-08-03 04:55:18 +0300

    Webpack config and more precise names.
    Inherit `app.ejs` template in corresponding legacy pages, but load
    different script chunks for each.
    - `index.html` will request `menu.bundle.js`,
    - `client.html` will request `game.bundle.js`.
    
    This way, user won't be overwhelmed with loading whole game just for
    servers listing.

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,69 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        ui: './webAO/ui.js',
-        client: './webAO/client.js',
-        master: './webAO/master.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'webAO'),
-        filename: '[name].b.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  [
-                    '@babel/preset-env', {
-                      useBuiltIns: 'usage',
-                      targets: 'defaults',
-                      corejs: 3
-                    }
-                  ]
-                ]
-              }
-            }
+  entry: {
+    menu: 'src/menu.js',
+    game: 'src/game.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+	commons: {
+	  chunks: 'initial',
+	  minChunks: 2,
+	  minSize: 0
+	},
+	vendors: {
+	  chunks: 'all',
+	  priority: 10
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env', {
+                  useBuiltIns: 'usage',
+                  targets: 'defaults',
+                  corejs: 3
+                }
+              ]
+            ]
           }
-        ]
-      },
-
-    devtool: 'source-map'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: true,
+      template: 'src/app.ejs',
+      filename: `public/index.html`,
+      chunks: ['menu']
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: true,
+      template: 'src/app.ejs',
+      filename: `public/client.html`,
+      chunks: ['game']
+    })
+  ],
+  devtool: 'source-map'
 };
+
